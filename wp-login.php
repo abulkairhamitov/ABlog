044dd3c9c27acbc222c9044fa2416ae09353dbe8
abulkairhamitov <46451501+abulkairhamitov@users.noreply.github.com>
2019-06-20 11:17:45 +0600

    Fixing CSS

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Войти &lsaquo; ABlog &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://abulkairhamitov.github.io/ABlog/wp-includes/css/dashicons.min.css?ver=5.2.1' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://abulkairhamitov.github.io/ABlog/wp-includes/css/buttons.min.css?ver=5.2.1' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://abulkairhamitov.github.io/ABlog/wp-admin/css/forms.min.css?ver=5.2.1' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://abulkairhamitov.github.io/ABlog/wp-admin/css/l10n.min.css?ver=5.2.1' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://abulkairhamitov.github.io/ABlog/wp-admin/css/login.min.css?ver=5.2.1' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost/https:/abulkairhamitov.github.io/ABlog/wp-includes/css/dashicons.min.css?ver=5.2.1' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost/https:/abulkairhamitov.github.io/ABlog/wp-includes/css/buttons.min.css?ver=5.2.1' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost/https:/abulkairhamitov.github.io/ABlog/wp-admin/css/forms.min.css?ver=5.2.1' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost/https:/abulkairhamitov.github.io/ABlog/wp-admin/css/l10n.min.css?ver=5.2.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost/https:/abulkairhamitov.github.io/ABlog/wp-admin/css/login.min.css?ver=5.2.1' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://ru.wordpress.org/">Сайт работает на WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="/https://abulkairhamitov.github.io/ABlog/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="http://localhost/https:/abulkairhamitov.github.io/ABlog/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Имя пользователя или e-mail<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -40,7 +40,7 @@
 	</form>
 
 			<p id="nav">
-					<a href="/https://abulkairhamitov.github.io/ABlog/wp-login.php?action=lostpassword">Забыли пароль?</a>
+					<a href="http://localhost/https:/abulkairhamitov.github.io/ABlog/wp-login.php?action=lostpassword">Забыли пароль?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,7 +57,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="/https://abulkairhamitov.github.io/ABlog/">
+			<p id="backtoblog"><a href="http://localhost/https:/abulkairhamitov.github.io/ABlog/">
 		&larr; Назад к сайту &laquo;ABlog&raquo;	</a></p>
 			
 	</div>
